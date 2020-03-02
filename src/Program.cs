09b9556099261b4f07f00c5483ba721d8c9b9ee0
Nitish <sharma.nitish@live.com>
2020-03-01 23:26:16 -0500

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ctci_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ht = new NHashTable<string, string>(20);
+            ht.Add("1", "item 1");
+            ht.Add("2", "item 2");
+            ht.Add("ajsd", "jkajdkajs");
+
+            
+            Console.WriteLine($"one = {ht.Find("1")}");
+            Console.WriteLine($"two = {ht.Find("2")}");
+            ht.Remove("1");
+            Console.WriteLine($"string three = {ht.Find("1")}");
+
+        }
+    }
+}
---
+++ src/chapter-1/data-structures/hash-table.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace ctci_csharp
+{
+    public class NHashTable<K, V>
+    {
+        private readonly int _size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+
+        public NHashTable(int size)
+        {
+            this._size = size;
+            items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+
+        public V Find(K key)
+        {
+            int position = GetArrayPosition(key);
+            var linkedList = GetLinkedList(position);
+            foreach (var item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                    return item.Value;
+            }
+
+            return default(V);
+        }
+
+        public void Add(K key, V value)
+        {
+            var position = GetArrayPosition(key);
+            var linkedList = GetLinkedList(position);
+            var item = new KeyValue<K, V>()
+            {
+                Key = key,
+                Value = value
+            };
+            linkedList.AddLast(item);
+        }
+
+        public void Remove(K key)
+        {
+            var position = GetArrayPosition(key);
+            var linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            var foundItem = default(KeyValue<K, V>);
+            foreach (var item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % this._size;
+            return Math.Abs(position);
+        }
+
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            var linkedList = this.items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+
+
+    public class KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+}
